c27ee0d2446bc8b07f3924a49ac3eb50b61845f8
JulienDuf <dufresne_julien@hotmail.ca>
2016-01-20 21:34:29 -0500

    Menu

--- TD1/seances.js
+++ TD1/seances.js
@@ -2,9 +2,18 @@
  * Created by julien on 1/8/16.
  */
 
-var app = angular.module('menuApp', []);
-    app.controller('menuController', function($scope, $http) {
+var menuApp = angular.module('menuApp', []);
+    menuApp.controller('menuController', function($scope, $http) {
 
         $http.get("menu.json")
             .then(function(response) {$scope.menuList = response.data.Navigation;})
+
+    });
+
+var seanceApp = angular.module('seanceApp', []);
+    seanceApp.controller('seanceController', function($scope, $http) {
+
+        $http.get("seances.json")
+            .then(function (response) {
+                $scope.seanceList = response.data;})
     });
