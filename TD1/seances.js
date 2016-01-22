730569f25cd7520fd8a8f438e84906446ac58846
Nicolas Coutu <nico.coutu@hotmail.com>
2016-01-22 15:32:44 -0500

    Travail du lab

--- TD1/seances.js
+++ TD1/seances.js
@@ -10,10 +10,13 @@ var menuApp = angular.module('menuApp', []);
 
     });
 
-var seanceApp = angular.module('seanceApp', []);
-    seanceApp.controller('seanceController', function($scope, $http) {
-
-        $http.get("seances.json")
-            .then(function (response) {
-                $scope.seanceList = response.data;})
+var app = angular.module('app', []);
+    app.controller('seancesCtrl', function($scope, $http, $sce) {
+        $http.get("seances.json").then(function(response) {
+            $scope.seances = angular.forEach(response.data, function (seance, index, tableau) {
+                tableau[index] = angular.forEach(seance, function(valeur, cle, tableau) {
+                    tableau[cle] = $sce.trustAsHtml(valeur);
+                });
+            });
+        });
     });
