bd2d7e2be9d4eaf90afa7915a9a8d16242f65e12
JulienDuf <dufresne_julien@hotmail.ca>
2016-01-28 10:58:10 -0500

    seances.json load now

--- TD1/seances.js
+++ TD1/seances.js
@@ -3,15 +3,11 @@
  */
 
 var menuApp = angular.module('menuApp', []);
-    menuApp.controller('menuController', function($scope, $http) {
+    menuApp.controller('menuController', function($scope, $http, $sce) {
 
         $http.get("menu.json")
             .then(function(response) {$scope.menuList = response.data.Navigation;})
 
-    });
-
-var app = angular.module('app', []);
-    app.controller('seancesCtrl', function($scope, $http, $sce) {
         $http.get("seances.json").then(function(response) {
             $scope.seances = angular.forEach(response.data, function (seance, index, tableau) {
                 tableau[index] = angular.forEach(seance, function(valeur, cle, tableau) {
